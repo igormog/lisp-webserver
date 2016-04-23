d4f0d3d32f09d10ffa6add0f1875add4a8486b8a
Igor Mogilin <igormogn@gmail.com>
2016-04-23 13:00:52 +0300

    worker-thread

--- web/web.lisp
+++ web/web.lisp
@@ -56,3 +56,40 @@
 	     )
 	   (release-lock *worker-mutex*)
 	   t))))
+
+(defun worker-thread (request-id socket idle-workers)
+  ;; Process request if it is not nil
+  (if request-id
+      (http-worker request-id socket))
+  (acquire-lock *worker-mutex*)
+  (cond
+    ;; Process request from queue
+    (*request-queue* 
+     (let ((request nil))
+       (setq request (car *request-queue*))
+       (setq *request-queue* (cdr *request-queue*))
+       (release-lock *worker-mutex*)
+       (worker-thread (car request) (cdr request) idle-workers)))
+    ;; If not enough idle workers, make worker idle
+    ((< *idle-workers-num* idle-workers)
+     (let ((condition (make-condition-variable))
+	   (idle-lock (make-lock))
+	   (request nil))
+       (push (cons condition (current-thread)) *idle-workers*)
+       (incf *idle-workers-num*)
+       (release-lock *worker-mutex*)
+       (list-workers)
+       (with-lock-held (idle-lock)
+	 (condition-wait condition idle-lock))
+       (with-lock-held (*worker-mutex*)
+	 (setq *idle-workers* (cdr *idle-workers*))
+	 (decf *idle-workers-num*)
+	 (setq request (car *request-queue*))	
+	 (setq *request-queue* (cdr *request-queue*)))
+       (worker-thread (car request) (cdr request) idle-workers)))
+    ;; else
+    (t (setq *workers* (remove (current-thread) *workers*))
+       (decf *worker-num*)
+       (release-lock *worker-mutex*))
+    )
+  )
